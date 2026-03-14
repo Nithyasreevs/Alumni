99c358f6c747d27151a8d89f45630dbddce6a9e1
necaaa-ui <necaaa@nec.edu.in>
2026-03-14 23:06:40 +0530

    fix vite base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/alumnimain/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
@@ -15,4 +15,3 @@ export default defineConfig({
     },
   },
 })
-
